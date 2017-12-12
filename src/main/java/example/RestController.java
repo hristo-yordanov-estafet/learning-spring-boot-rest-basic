ade183695156994508100f9a4a7834fd856c5f02
Hristo Yordanov <hristo.yordanov@estafet.com>
2017-12-12 16:26:38 +0200

    update request mapping for batches

--- src/main/java/example/RestController.java
+++ src/main/java/example/RestController.java
@@ -1,20 +1,25 @@
 package example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/batch")
+@RequestMapping("/batches")
 public class RestController {
 
 
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody Batch getBatch(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
-        return new Batch(15488, Arrays.asList("CRM", "TLR"));
+    public @ResponseBody List<Batch> getBatch() {
+    	List<Batch> list = new ArrayList<Batch>(2);
+    	list.add(new Batch(15488, Arrays.asList("CRM", "TLR")));
+    	list.add(new Batch(15489, Arrays.asList("FUN", "EIP")));
+        return list;
     }
 
 }
